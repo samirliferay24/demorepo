0bf7dad0bec3d2481ad03efc9561e35ffccbbb44
Samir <Samir@Sameer>
2016-04-08 19:58:58 +0530

    method written

---
+++ contact-details-portlet/docroot/WEB-INF/src/com/lic/contact/ContactDetailPortlet.java
@@ -0,0 +1,15 @@
+package com.lic.contact;
+
+import com.liferay.util.bridges.mvc.MVCPortlet;
+
+/**
+ * Portlet implementation class ContactDetailPortlet
+ */
+public class ContactDetailPortlet extends MVCPortlet {
+ 
+public void action()
+{
+System.out.println("hello");	
+}
+	
+}
